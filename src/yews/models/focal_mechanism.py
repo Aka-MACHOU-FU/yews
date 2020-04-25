1b78fd3f3c161931161f3a7b43a392a81a5af9cb
Aka-MACHOU-FU <55723255+Aka-MACHOU-FU@users.noreply.github.com>
2020-04-25 19:35:09 -0400

    change first model to fully connected

--- src/yews/models/focal_mechanism.py
+++ src/yews/models/focal_mechanism.py
@@ -24,129 +24,16 @@ class FmV1(nn.Module):
         # (3,7) 3>71 7>18000 in the end use (3,3).
 
         # 71,9000 -> 71,4507
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=(3,17), stride=1, padding=(2,16), bias=False),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            # nn.Sigmoid(),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2))
-        )
-
-        # 71,4507 -> 71,2260
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=(3,15), stride=1, padding=(2,14), bias=False),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            # nn.Sigmoid(),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2))
-        )
-
-        # 71,2260 -> 71,1135
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=(3,13), stride=1, padding=(2,12), bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2))
-        )
-
-        # 71,1135 -> 71,572
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=(3,11), stride=1, padding=(2,10), bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2))
-        )
-
-        # 71,572 -> 71,289
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=(3,9), stride=1, padding=(2,8), bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2))
-        )
-
-        # 71,289 -> 71,143
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=(3,7), stride=1, padding=2, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2))
-        )
-
-        # 71,143 -> 71,71
-        self.layer7 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=(3,5), stride=1, padding=2, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2))
-        )
-
-        # 71,71 -> 35,35
-        self.layer8 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        # 35,35 -> 17,17
-        self.layer9 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        
-        # 17,17 -> 8,8
-        self.layer10 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        
-        
-        # 8,8 -> 4,4
-        self.layer11 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        
-        # 4,4 -> 2,2
-        self.layer12 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        # 2,2 -> 1,1
-        self.layer13 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        self.fc = nn.Linear(64 * 1 * 1, 3)
+        self.layer1 = nn.linear(3*71*9000,10)
+        self.layer2 = nn.linear(10,5)
+        self.layer3 = nn.linear(5,3)
+     
+        self.fc = nn.Linear(3, 1)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        out = self.layer6(out)
-        out = self.layer7(out)
-        out = self.layer8(out)
-        out = self.layer9(out)
-        out = self.layer10(out)
-        out = self.layer11(out)
-        out = self.layer12(out)
-        out = self.layer13(out)
-        out = out.view(out.size(0), -1)
         out = self.fc(out)
 
         return out
